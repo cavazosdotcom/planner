43de054f58d8593e3f10e41fd1b8332ab24c7f48
cavazosdotcom <cavazosdotcom@gmail.com>
2021-12-13 16:03:30 -0800

    added notes, readme, and fixed loop to also include 5pm time-block

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # planner
-Creating a working planner that keeps track of time and events in real time
+
+Creating a working planner that keeps track of time and events in real time, and saves the events on page reload
+
+# Links
+
+[planner Github URL](https://cavazosdotcom.github.io/planner/)
+
+[planner Github Repo URL](https://github.com/cavazosdotcom/planner)
+
+# Picture
+
+![Work Day Planner](assets/img/planner.png)
+
+# Work Flow
+
+- I would say this project was easier for me than the previous coding quiz. Besides the moment.js implementation, everything here I got some exposure with in the previous JS we did. It wasn't learning it for the first time, it was taking what we knew and doing more. The concept of parents, siblings, and children we cool to explore so I could properly use the this property. The different functions I would say are more complex than previous work but it was much shorter. Jquery did help a lot in shortening the actual code. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-// var time;
-// var value;
-// var today;
-
+// selects save-btn class and on click calls this function
+// time variable set to the id of the save button clicked 
+// the value is set to whatever is inside the text box with the description class
+// Both set to local storage
 $('.save-btn').on('click', function(){
     const time = $(this).attr('id');
     console.log(time);
@@ -14,19 +14,32 @@ $('.save-btn').on('click', function(){
 });
 
 
-
-for (let i = 9; i < 12; i++){
+// between 9 and 5(17 military time), select each time id # and the description
+// then get the value with the correlating time in local storage so the
+// events shown stay upon page reload instad of clearing
+for (let i = 9; i <= 17; i++){
     $(`#${i}`).siblings('.description').val(localStorage.getItem(`${i}`));
 };
 
 
+// creates function for the current time in the jumbotron
+// var today is set to the current moment from moment.js
+// then we select the current day id and give the format to our moment.js time value
 var currentTime = function (){
     const today = moment();
     $('#current-day').text(today.format('MMMM Do YYYY, h:mm:ss a'));
 }; 
+// On page load the time from moment.js should count by seconds
 setInterval(currentTime, 1000);
 
 
+// function to change color of each hour block in the planner based on the current time of day
+// our current hour is based on moment.js hours
+// selects each time-block class and for each one, we run the function
+// we then create our own plannerHour to compare to the moment.js current hour
+// then we select the time-block divs and dig to select the id inside the child
+// of our parent div that is a button element
+// We compare the real moment.js time and each hour block to add and remove our given css styling
 function hourColor() {
     var currentHour = moment().hours();
     console.log(currentHour);
